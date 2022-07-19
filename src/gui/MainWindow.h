8fa1263325b84fe842e3b6f777b9fc8cac98bbf0
Joseph Black <17530522+jbinvnt@users.noreply.github.com>
2022-07-18 23:42:35 -0500

    Add a dialog for selecting a shader directory

--- src/gui/MainWindow.h
+++ src/gui/MainWindow.h
@@ -327,6 +327,7 @@ public slots:
   void viewOrthogonal();
   void viewTogglePerspective();
   void viewResetView();
+  void setShader();
   void viewAll();
   void editorContentChanged();
   void selectObject(QPoint coordinate);
